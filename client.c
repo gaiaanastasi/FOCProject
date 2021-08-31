4f42b0fc989830ab47b40f33b806fb83e265d99e
matteoGuidotti <89739582+matteoGuidotti@users.noreply.github.com>
2021-08-31 11:10:22 +0200

    client and crypto update

--- client.c
+++ client.c
@@ -1,26 +1,36 @@
 //CLIENT
 
+#include <arpa/inet.h>
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include "crypto.c"
+#include "utility.c"
 
 const int port_address = 4242;
 const char ip_address[16] = "127.0.0.1"
 const char welcomeMessage[256] = "Hi! This is a secure messaging system \n Type: \n (1) to see who's online \n (2) to send a request to talk (3) to log out\n\n What do you want to do? ";
 
 int main(){
-	int sd
-	unsigned command;
+	int socket;
+	unsigned command;			//command tyoed by the user
+	char myNonce[DIM_NONCE];	//it will store the nonce created by the client
 
 	//socket creation and instantiation
-	sd = socket(AF_INET, SOCK_STREAM, 0);
+	socket = socket(AF_INET, SOCK_STREAM, 0);
 	memset(&srv_addr, 0, sizeof(srv_addr)); // Pulizia 
     srv_addr.sin_family = AF_INET;
     srv_addr.sin_port = htons(atoi(port_address));
     inet_pton(AF_INET, ip_address, &srv_addr.sin_addr);
-    ret = connect(sd, (struct sockaddr*)&srv_addr, sizeof(srv_addr));
+    ret = connect(socket, (struct sockaddr*)&srv_addr, sizeof(srv_addr));
 	if(ret < 0){
 		perror("An error occured during the connection phase \n");
 		exit(-1);
 	}
+
+	//authentication with the server
+	generateNonce(myNonce);
+
 
 	printf("%s", welcomeMessage);
 	while(1){
---
+++ crypto.c
@@ -0,0 +1,12 @@
+#include <openssl/rand.h>
+
+#define DIM_NONCE 16
+
+//function that generate a nonce of DIM_NONCE bit
+void generateNonce(char* nonce){
+	if(RAND_poll() != 1)
+		perror("error occured during RAND_poll()");
+	if(RAND_bytes(nonce, DIM_NONCE) != 1)
+		perror("error occured during generation of the nonce");
+	printf("the nonce has been generated\n");
+}
